03c843673dcf43758b4df8b4b53ecb661b08629c
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-10 04:40:11 +0530

    Added:Emp Calculating Emp Parttime wage

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,11 +1,14 @@
 public class EmpWageBuilderUC1 {
 	public static void main(String[] args){
-		int IS_FULL_TIME=1;
+		int IS_FULL_TIME=2;
+		int IS_PART_TIME=1;
 		int EMP_RATE_PER_HR=20;
 		int empHrs;
-		double empCheck=Math.floor(Math.random() * 10) % 2;
+		double empCheck=Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_FULL_TIME)
 			empHrs=8;
+		else if(empCheck == IS_PART_TIME)
+			empHrs=4;
 		else
 			empHrs=0;
 		int totalSalary=empHrs*EMP_RATE_PER_HR;

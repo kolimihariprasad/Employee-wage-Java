aeaea4e968b92e8cfc488d5e8b48aabf7634ba50
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-10 03:41:28 +0530

    Added:Emp Calculating Emp Daily wage

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,10 +1,14 @@
 public class EmpWageBuilderUC1 {
 	public static void main(String[] args){
 		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HR=20;
+		int empHrs;
 		double empCheck=Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Present");
+			empHrs=8;
 		else
-			System.out.println("Absent");
+			empHrs=0;
+		int totalSalary=empHrs*EMP_RATE_PER_HR;
+		System.out.println(totalSalary);
 	}
 }

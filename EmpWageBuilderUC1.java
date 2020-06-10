e8ac940005e09b89e784a075ed1119c0773bed30
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-10 05:31:15 +0530

    Added:Calculating wages for a month and added monthly Working hours condition

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,10 +1,12 @@
 public class EmpWageBuilderUC1 {
 	public static void main(String[] args){
+		//Constants
 		int IS_FULL_TIME=2;
 		int IS_PART_TIME=1;
 		int EMP_RATE_PER_HR=20;
 		int TOTAL_WORKING_DAYS=20;
 		int TOTAL_WORKING_HOURS=100;
+		//Variables
 		int empHrs;
 		int totalHrs=0;
 		int NumWorkingDays=0;
